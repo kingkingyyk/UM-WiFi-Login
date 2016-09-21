71f0e9abd7a4ada4e3d50d6ba73b92810b37c091
Yap Yee King <king8793yyk@hotmail.com>
2016-09-21 18:56:47 +0800

    Wed 21 Sep 2016 18:55 commit by king
    - Improved memory usage
    - Invalid credential will not be retried until the credential is
    changed.

--- src/umwifilogin/umwifilogin.java
+++ src/umwifilogin/umwifilogin.java
@@ -14,9 +14,16 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 import javax.swing.ImageIcon;
 import javax.swing.UIManager;
+
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlCheckBoxInput;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
@@ -29,9 +36,12 @@ public class umwifilogin {
 	public static ImageIcon LogoIcon=new ImageIcon(Toolkit.getDefaultToolkit().getClass().getResource("/img-logo.png"));
 	public static Image LogoImage=LogoIcon.getImage();
 	public static File ConfigFile=new File(System.getProperty("java.io.tmpdir")+"\\UMWiFiLogin.txt");
+	public static String Version="v1.1";
 	public static String username;
 	public static String password;
+	public static boolean lastLoginFailed=false;
 	private static TrayIcon SysTrayIcn;
+	
 	
 	public static void main (String [] args) throws Exception {
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -68,6 +78,7 @@ public class umwifilogin {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			lastLoginFailed=false;
 		} else if (startup) {
 			System.exit(0);
 		}
@@ -81,6 +92,10 @@ public class umwifilogin {
 		MenuItem watermark=new MenuItem("By kingkingyyk");
 		watermark.setEnabled(false);
 		popup.add(watermark);
+		
+		MenuItem versionMark=new MenuItem(Version);
+		versionMark.setEnabled(false);
+		popup.add(versionMark);
 		
 		MenuItem changeCredential=new MenuItem("Change account");
 		changeCredential.addActionListener(new ActionListener() {
@@ -116,29 +131,49 @@ public class umwifilogin {
 		Thread t=new Thread() {
 			public void run() {
 				while (true) {
-					WebClient client=new WebClient();
-					try {
-						HtmlPage page=client.getPage("http://www.google.com.my");
-						
-						if (page.getBaseURL().toString().contains("umwifilogin")) {
-							HtmlForm f=page.getForms().get(0);
-							HtmlTextInput usernameField=f.getInputByName("user");
-							HtmlPasswordInput passwordField=f.getInputByName("password");
-							usernameField.setValueAttribute(username);
-							passwordField.setValueAttribute(password);
-							HtmlCheckBoxInput box=f.getInputByName("visitor_accept_terms");
-							box.setChecked(true);
-							page=f.getInputByValue("Log In").click();
-							if (page.getBaseURL().toString().contains("umwifilogin")) {
-								SysTrayIcn.displayMessage("UM WiFi Auto Login", "Log in failed. Please check your credential. :)", TrayIcon.MessageType.ERROR);
-							} else {
-								SysTrayIcn.displayMessage("UM WiFi Auto Login", "Just logged in. :)", TrayIcon.MessageType.INFO);
+					if (!lastLoginFailed) {
+						try {
+							URL url=new URL("http://www.google.com");
+							HttpURLConnection.setFollowRedirects(true);
+							HttpURLConnection conn=(HttpURLConnection) url.openConnection();
+							conn.connect();
+							InputStream is=conn.getInputStream();
+							BufferedReader br=new BufferedReader(new InputStreamReader(is));
+							ArrayList<String> htmlCode=new ArrayList<>();
+							String s;
+							while ((s=br.readLine())!=null && htmlCode.size()<3) {
+								htmlCode.add(s);
 							}
+							br.close();
+							is.close();
+							conn.disconnect();
+							SysTrayIcn.displayMessage("UM WiFi Auto Login", "UM WiFi detected, logging in...", TrayIcon.MessageType.INFO);
+							if (htmlCode.size()>=3 && htmlCode.get(2).contains("arubalp=")) {
+								WebClient client=new WebClient();
+								HtmlPage page=client.getPage("http://www.google.com.my");
+								HtmlForm f=page.getForms().get(0);
+								HtmlTextInput usernameField=f.getInputByName("user");
+								HtmlPasswordInput passwordField=f.getInputByName("password");
+								usernameField.setValueAttribute(username);
+								passwordField.setValueAttribute(password);
+								HtmlCheckBoxInput box=f.getInputByName("visitor_accept_terms");
+								box.setChecked(true);
+								HtmlPage page2=f.getInputByValue("Log In").click();
+								page.cleanUp();
+								if (page2.getBaseURL().toString().contains("umwifilogin")) {
+									SysTrayIcn.displayMessage("UM WiFi Auto Login", "Log in failed. Please check your credential. :)", TrayIcon.MessageType.ERROR);
+									lastLoginFailed=true;
+								} else {
+									SysTrayIcn.displayMessage("UM WiFi Auto Login", "Login success! :)", TrayIcon.MessageType.INFO);
+									lastLoginFailed=false;
+								}
+								page2.cleanUp();
+								client.close();
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
 						}
-					} catch (Exception e) {
-						e.printStackTrace();
 					}
-					client.close();
 					try {
 						Thread.sleep(10000);
 					} catch (InterruptedException e) {}
