ff26be6fd24f5c097f322ad4746943a052ddb82b
Yap Yee King <king8793yyk@hotmail.com>
2016-12-28 08:18:34 +0800

    Wed 28 Dec 2016 08:17 commit by king
    v1.2.2
    ----------
    Functionality :
    - Added menu to trigger login.

--- src/umwifilogin/umwifilogin.java
+++ src/umwifilogin/umwifilogin.java
@@ -29,7 +29,7 @@ public class umwifilogin {
 	public static ImageIcon LogoIcon=new ImageIcon(Toolkit.getDefaultToolkit().getClass().getResource("/img-logo.png"));
 	public static Image LogoImage=LogoIcon.getImage();
 	public static File ConfigFile=new File(System.getProperty("java.io.tmpdir")+"\\UMWiFiLogin.txt");
-	public static String Version="v1.2.1";
+	public static String Version="v1.2.2";
 	public static String username;
 	public static String password;
 	public static boolean lastLoginFailed=false;
@@ -49,7 +49,7 @@ public class umwifilogin {
 		}
 		
 		CreateTrayIcon();
-		StartLogin();
+		startLogin();
 	}
 	
 	public static void CredentialDialog(boolean startup){
@@ -101,6 +101,18 @@ public class umwifilogin {
 		});
 		popup.add(changeCredential);
 		
+		MenuItem login=new MenuItem("Trigger Login");
+		login.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				tryLogin();
+			}
+			
+		});
+		popup.add(login);
+		
+		
 		MenuItem exit=new MenuItem("Exit");
 		exit.addActionListener(new ActionListener() {
 
@@ -120,54 +132,11 @@ public class umwifilogin {
 		SysTrayIcn=icn;
 	}
 	
-	public static void StartLogin() {
+	public static void startLogin() {
 		Thread t=new Thread() {
 			public void run() {
 				while (true) {
-					if (!lastLoginFailed) {
-						try {
-							URL url=new URL("http://www.google.com");
-							HttpURLConnection.setFollowRedirects(true);
-							HttpURLConnection conn=(HttpURLConnection) url.openConnection();
-							conn.connect();
-							InputStream is=conn.getInputStream();
-							BufferedReader br=new BufferedReader(new InputStreamReader(is));
-							String s=br.readLine();
-							br.close();
-							is.close();
-							conn.disconnect();
-							if (s!=null && s.contains("unified.um.edu.my")) {
-								SysTrayIcn.displayMessage("UM WiFi Auto Login", "UM WiFi detected, logging in...", TrayIcon.MessageType.INFO);
-								url=new URL("http://unified.um.edu.my/eportal/InterFace.do?method=login&time=Tue%20Oct%2004%202016%2008:50:45%20GMT+0800%20(Malay%20Peninsula%20Standard%20Time)");
-								HttpURLConnection.setFollowRedirects(true);
-								conn=(HttpURLConnection) url.openConnection();
-								s=s.replace("<script>top.self.location.href='https://unified.um.edu.my/eportal/index.jsp?","").replaceAll("'</script>","");
-								s=s.replaceAll("=","%253D").replaceAll("&","%2526");
-								StringBuilder sb=new StringBuilder();
-								sb.append("userId="); sb.append(username); sb.append("&");
-								sb.append("password="); sb.append(password); sb.append("&");
-								sb.append("service="); sb.append("perdana.um.edu.my"); sb.append("&");
-								sb.append("queryString="); sb.append(s); sb.append("&");
-								sb.append("operatorPwd="); sb.append("&"); sb.append("validcode=");
-								s=sb.toString();
-								conn.setRequestMethod("POST");
-								conn.setRequestProperty("User-Agent","Mozilla/5.0");
-								conn.setRequestProperty("Accept-Language","en-US, en;q=0.5");
-								conn.setDoOutput(true);
-								DataOutputStream wr=new DataOutputStream(conn.getOutputStream());
-								wr.writeBytes(s);
-								wr.flush();
-								wr.close();
-								
-								is=conn.getInputStream();
-								br=new BufferedReader(new InputStreamReader(is));
-								br.close();
-								is.close();
-							}
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
+					if (!lastLoginFailed) tryLogin();
 					try {
 						Thread.sleep(10000);
 					} catch (InterruptedException e) {}
@@ -177,4 +146,48 @@ public class umwifilogin {
 		t.start();
 	}
 	
+	public static void tryLogin() {
+		try {
+			URL url=new URL("http://www.google.com");
+			HttpURLConnection.setFollowRedirects(true);
+			HttpURLConnection conn=(HttpURLConnection) url.openConnection();
+			conn.connect();
+			InputStream is=conn.getInputStream();
+			BufferedReader br=new BufferedReader(new InputStreamReader(is));
+			String s=br.readLine();
+			br.close();
+			is.close();
+			conn.disconnect();
+			if (s!=null && s.contains("unified.um.edu.my")) {
+				SysTrayIcn.displayMessage("UM WiFi Auto Login", "UM WiFi detected, logging in...", TrayIcon.MessageType.INFO);
+				url=new URL("http://unified.um.edu.my/eportal/InterFace.do?method=login&time=Tue%20Oct%2004%202016%2008:50:45%20GMT+0800%20(Malay%20Peninsula%20Standard%20Time)");
+				HttpURLConnection.setFollowRedirects(true);
+				conn=(HttpURLConnection) url.openConnection();
+				s=s.replace("<script>top.self.location.href='https://unified.um.edu.my/eportal/index.jsp?","").replaceAll("'</script>","");
+				s=s.replaceAll("=","%253D").replaceAll("&","%2526");
+				StringBuilder sb=new StringBuilder();
+				sb.append("userId="); sb.append(username); sb.append("&");
+				sb.append("password="); sb.append(password); sb.append("&");
+				sb.append("service="); sb.append("perdana.um.edu.my"); sb.append("&");
+				sb.append("queryString="); sb.append(s); sb.append("&");
+				sb.append("operatorPwd="); sb.append("&"); sb.append("validcode=");
+				s=sb.toString();
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("User-Agent","Mozilla/5.0");
+				conn.setRequestProperty("Accept-Language","en-US, en;q=0.5");
+				conn.setDoOutput(true);
+				DataOutputStream wr=new DataOutputStream(conn.getOutputStream());
+				wr.writeBytes(s);
+				wr.flush();
+				wr.close();
+				
+				is=conn.getInputStream();
+				br=new BufferedReader(new InputStreamReader(is));
+				br.close();
+				is.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
